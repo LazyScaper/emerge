b9fc1bb8d62ceefb744874df5be146a54be170b3
Jake <40252392+JamblaInc@users.noreply.github.com>
2025-06-10 21:45:20 +0100

    [Jake] Refactor to use a better graph struct

--- src/graph_builder.rs
+++ src/graph_builder.rs
@@ -1,5 +1,6 @@
 use csv::ReaderBuilder;
 use serde::Deserialize;
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::path::Path;
 
@@ -17,14 +18,64 @@ struct Country {
 
 #[derive(Debug, Deserialize)]
 pub struct CountryNode {
+    id: usize,
+    data: CountryData,
+    outgoing_edges: HashSet<usize>,
+    incoming_edges: HashSet<usize>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct CountryData {
     name: String,
     first_letter: char,
     last_letter: char,
-    children: Vec<CountryNode>,
 }
 
 pub(crate) struct Graph {
     pub(crate) nodes: Vec<CountryNode>,
+    pub(crate) node_lookup: HashMap<String, usize>,
+}
+
+impl Graph {
+    pub fn new() -> Self {
+        Self {
+            nodes: Vec::new(),
+            node_lookup: HashMap::new(),
+        }
+    }
+
+    pub fn add_node(&mut self, name: String, node_data: CountryData) {
+        let id = self.nodes.len();
+
+        self.node_lookup.insert(name, id);
+        self.nodes.push(CountryNode {
+            id,
+            data: node_data,
+            outgoing_edges: HashSet::new(),
+            incoming_edges: HashSet::new(),
+        });
+    }
+
+    pub fn add_edge(&mut self, from: usize, to: usize) {
+        if from == to {
+            return;
+        }
+
+        self.nodes.get_mut(from).unwrap().outgoing_edges.insert(to);
+        self.nodes.get_mut(to).unwrap().incoming_edges.insert(from);
+    }
+    pub fn add_edge_by_name(&mut self, from_name: &str, to_name: &str) {
+        if from_name.eq(to_name) {
+            return;
+        }
+
+        if let (Some(&from_id), Some(&to_id)) = (
+            self.node_lookup.get(from_name),
+            self.node_lookup.get(to_name),
+        ) {
+            self.add_edge(from_id, to_id);
+        }
+    }
 }
 
 pub fn country_chain_finder() {
@@ -50,11 +101,10 @@ pub fn country_chain_finder() {
         let first_letter = country.name.chars().next().unwrap();
         let last_letter = country.name.chars().last().unwrap();
 
-        starting_candidates.push(CountryNode {
+        starting_candidates.push(CountryData {
             name: country.name,
             first_letter,
             last_letter,
-            children: Vec::new(),
         });
 
         // starting_candidates.clo
@@ -68,12 +118,25 @@ pub fn country_chain_finder() {
 }
 
 pub fn build_graph() -> Graph {
-    Graph {
-        nodes: Vec::from([CountryNode {
-            name: "Test Node".parse().unwrap(),
-            first_letter: "T".parse().unwrap(),
-            last_letter: "e".parse().unwrap(),
-            children: Vec::new(),
-        }]),
-    }
+    let mut graph = Graph::new();
+
+    graph.add_node(
+        "Albania".to_string(),
+        CountryData {
+            name: "Albania".to_string(),
+            last_letter: 'a',
+            first_letter: 'a',
+        },
+    );
+    graph.add_node(
+        "Cambodia".to_string(),
+        CountryData {
+            name: "Cambodia".to_string(),
+            last_letter: 'c',
+            first_letter: 'a',
+        },
+    );
+    graph.add_edge_by_name("Cambodia", "Albania");
+
+    graph
 }
