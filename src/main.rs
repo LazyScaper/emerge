d35db9551e089df9f76115e2e37a8376a2a5dc79
Jake <40252392+JamblaInc@users.noreply.github.com>
2025-06-10 22:49:31 +0100

    [Jake] Spawn nodes in random locations

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use macroquad::prelude::*;
 
 mod graph_builder;
 mod graph_renderer;
+use random::prelude::*;
 
 fn window_conf() -> Conf {
     Conf {
@@ -22,12 +23,15 @@ fn physics_update(world: World) {}
 async fn main() {
     let mut world = World::new();
     let graph = build_graph();
+    let mut rng = random::rng();
 
     for _node in graph.nodes {
-        // TODO: construct each renderable node from the graph
         let renderable_node = (
             Mass { mass: 1.0 },
-            Position { x: 100.0, y: 100.0 },
+            Position {
+                x: rng.random_range(0.0..screen_width()),
+                y: rng.random_range(0.0..screen_height()),
+            },
             Size { radius: 15.0 },
             NodeColor { color: BLACK },
         );
