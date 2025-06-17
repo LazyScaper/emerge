90c0ef1bbea6e14b12636ac863f2772d312efa32
Jake <40252392+JamblaInc@users.noreply.github.com>
2025-06-17 22:32:23 +0100

    [Jake] extract method to apply forces on the nodes

--- src/main.rs
+++ src/main.rs
@@ -49,39 +49,47 @@ fn physics_update(world: &mut World) {
                 node_data.get(&edge_destination_node_id),
             ) {
                 (Some(source_node_position), Some(destination_node_position)) => {
-                    if let Some((_found_node, (mut force, node_id))) = world
-                        .query::<(&mut Force, &NodeId)>()
-                        .iter()
-                        .find(|(entity, (force, node_id))| edge_source_node_id == node_id.id)
-                    {
-                        let dx = source_node_position.x - destination_node_position.x;
-                        let dy = source_node_position.y - destination_node_position.y;
-
-                        let current_length = (dx * dx + dy * dy).sqrt();
-                        let displacement_from_rest = current_length - SPRING_RESTING_LENGTH;
-
-                        force.x = -SPRING_CONSTANT * displacement_from_rest * (dx / current_length);
-                        force.y = -SPRING_CONSTANT * displacement_from_rest * (dy / current_length);
-                    }
-
-                    if let Some((_found_node, (mut force, node_id))) = world
-                        .query::<(&mut Force, &NodeId)>()
-                        .iter()
-                        .find(|(entity, (force, node_id))| edge_destination_node_id == node_id.id)
-                    {
-                        let dx = destination_node_position.x - source_node_position.x;
-                        let dy = destination_node_position.y - source_node_position.y;
-
-                        let current_length = (dx * dx + dy * dy).sqrt();
-                        let displacement_from_rest = current_length - SPRING_RESTING_LENGTH;
-
-                        force.x = -SPRING_CONSTANT * displacement_from_rest * (dx / current_length);
-                        force.y = -SPRING_CONSTANT * displacement_from_rest * (dy / current_length);
-                    }
+                    apply_forces_between_nodes(
+                        world,
+                        edge_source_node_id,
+                        destination_node_position,
+                        source_node_position,
+                    );
+                    apply_forces_between_nodes(
+                        world,
+                        edge_destination_node_id,
+                        source_node_position,
+                        destination_node_position,
+                    );
                 }
                 _ => {}
             }
         };
+    }
+}
+
+fn apply_forces_between_nodes(
+    world: &mut World,
+    node_id_to_match: usize,
+    source_node_position: &Position,
+    destination_node_position: &Position,
+) {
+    match world
+        .query::<(&mut Force, &NodeId)>()
+        .iter()
+        .find(|(_entity, (_force, node_id))| node_id_to_match == node_id.id)
+    {
+        Some((_found_node, (mut force, _node_id))) => {
+            let dx = destination_node_position.x - source_node_position.x;
+            let dy = destination_node_position.y - source_node_position.y;
+
+            let current_length = (dx * dx + dy * dy).sqrt();
+            let displacement_from_rest = current_length - SPRING_RESTING_LENGTH;
+
+            force.x = -SPRING_CONSTANT * displacement_from_rest * (dx / current_length);
+            force.y = -SPRING_CONSTANT * displacement_from_rest * (dy / current_length);
+        }
+        None => {}
     }
 }
 
