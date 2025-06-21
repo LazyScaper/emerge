defb11c4b0f4f4189c44312c00459ff0ea7390d7
Jake <40252392+JamblaInc@users.noreply.github.com>
2025-06-21 20:50:16 +0100

    [Jake] Add `Simulator` struct to `simulator.rs` with basic graph generation logic

--- examples/simulator.rs
+++ examples/simulator.rs
@@ -1,4 +1,33 @@
 use emerge::graph::{default_window_conf, render_graph, Graph};
+use random::Rng;
+
+struct Simulator {
+    number_of_nodes: usize,
+    number_of_edges: usize,
+}
+
+impl Simulator {
+    fn new() -> Self {
+        Self {
+            number_of_nodes: 30,
+            number_of_edges: 30,
+        }
+    }
+
+    fn graph(&mut self) -> Graph {
+        let mut rng = random::rng();
+
+        self.number_of_edges = rng.random_range(..100);
+        self.number_of_nodes = rng.random_range(..100);
+
+        let graph = Graph::new();
+
+        for _ in 0..self.number_of_nodes {
+            // graph.add_node("Node".to_string(), "Node".to_string());
+        }
+        graph
+    }
+}
 
 #[macroquad::main(default_window_conf)]
 async fn main() {
