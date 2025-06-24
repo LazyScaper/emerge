57cc7265c48e5506e03555c7885422488de40ba0
Jake <40252392+JamblaInc@users.noreply.github.com>
2025-06-24 21:12:18 +0100

    [Jake] Complete factor_tree implementation

--- examples/factor_tree.rs
+++ examples/factor_tree.rs
@@ -3,30 +3,41 @@ use emerge::graph::{default_window_conf, render_graph, Graph};
 fn build_factor_tree(value: i32) -> Graph {
     let mut graph = Graph::new();
 
-    compute_factor_tree(value, &mut graph);
+    let mut visited_nodes = vec![];
+
+    graph.add_node(&value.to_string());
+
+    compute_factor_tree(&mut visited_nodes, value, &mut graph);
 
     graph
 }
 
-fn compute_factor_tree(value: i32, graph: &mut Graph) {
-    if value == 1 {
+fn compute_factor_tree(visited_nodes: &mut Vec<i32>, value: i32, graph: &mut Graph) {
+    if value == 1 || visited_nodes.contains(&value) {
         return;
     }
-
-    graph.add_node(&value.to_string());
 
     for number_to_check_as_factor in (2..(value as f32).sqrt() as i32 + 1).rev() {
         if value % number_to_check_as_factor == 0 {
             let first_factor = number_to_check_as_factor;
             let second_factor = (value / number_to_check_as_factor);
 
-            graph.add_node(&first_factor.to_string());
-            graph.add_node(&second_factor.to_string());
-            graph.add_directed_edge(&value.to_string(), &first_factor.to_string());
-            graph.add_directed_edge(&value.to_string(), &second_factor.to_string());
+            visited_nodes.push(value);
 
-            compute_factor_tree(first_factor, graph);
-            compute_factor_tree(second_factor, graph);
+            if first_factor != second_factor {
+                graph.add_node(&first_factor.to_string());
+                graph.add_node(&second_factor.to_string());
+                graph.add_directed_edge(&value.to_string(), &first_factor.to_string());
+                graph.add_directed_edge(&value.to_string(), &second_factor.to_string());
+
+                compute_factor_tree(visited_nodes, first_factor, graph);
+                compute_factor_tree(visited_nodes, second_factor, graph);
+            } else {
+                graph.add_node(&first_factor.to_string());
+                graph.add_directed_edge(&value.to_string(), &first_factor.to_string());
+
+                compute_factor_tree(visited_nodes, first_factor, graph);
+            }
             return;
         }
     }
@@ -34,7 +45,7 @@ fn compute_factor_tree(value: i32, graph: &mut Graph) {
 
 #[macroquad::main(default_window_conf)]
 async fn main() {
-    let graph = build_factor_tree(456);
+    let graph = build_factor_tree(34280);
 
     render_graph(graph).await;
 }
